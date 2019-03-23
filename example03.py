0d4b3088a48e35c5d90565f122cccede9374b386
Ilia <ilya-polozov@mail.ru>
2019-03-23 17:51:03 +0300

    generate new button and label

--- example03.py
+++ example03.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 '''
 Пример для первой лекции про TkInter
 
 Закрытие окошка в постинтерактивном режиме
 '''
 
-from tkinter import *
 
-def dump(*args):
-    print("DUMP:",args)
-
+from Tkinter import *
+from random import randint
 TKroot = Tk()
 TKroot.title("Hello")
 
@@ -21,13 +20,39 @@ root.columnconfigure(1, weight=2)
 root.rowconfigure(0, weight=10)
 root.rowconfigure(1, weight=1)
 
-Butt = Button(root, text="Butt ON")
-Butt.bind('<Button-1>', dump)
+
+
+colors = ["red", "green", "blue"]
+
+def _from_rgb(rgb):
+    """translates an rgb tuple of int to a tkinter friendly color code
+    """
+    return "#%02x%02x%02x" % rgb  
+
+
+def random_color():
+    Txt = Label(root, text="This is a label", bg=_from_rgb((randint(0, 255), randint(0, 255), randint(0, 255))))
+    Txt.grid(row=1, column=1, columnspan=100, sticky=E+W+N+S)
+
+
+
+def dump():
+    #print("DUMP:",args)
+    Butt2 = Button(root, text="New But",command = random_color)
+    #Butt2.bind('<Button-2>', dump)
+    Butt2.grid(row=1, column=0, sticky=E+W+S+N)
+    Txt = Label(root, text="This is a label", bg=_from_rgb((randint(0, 255), randint(0, 255), randint(0, 255))))
+    Txt.grid(row=1, column=1, columnspan=100, sticky=E+W+N+S)
+
+
+
+#result = dump(root)
+Butt = Button(root, text="Add", command = dump)
+#Butt.bind('<Button-1>', dump(root))
 Butt.grid(row=0, column=0, sticky=E+W+S+N)
-Exit = Button(root, text="Quit!", command=root.quit)
+Exit = Button(root, text="Exit", command=root.quit)
 Exit.grid(row=0, column=1, sticky=E+W+S+N)
-Txt = Label(root, text="This is a label", bg="PeachPuff")
-Txt.grid(row=1, column=0, columnspan=2, sticky=E+W+N)
+
 
 TKroot.mainloop()
 print("Done")
